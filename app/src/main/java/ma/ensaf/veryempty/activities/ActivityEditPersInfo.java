4b41e99b906df95c5be59c0b7f3b75e59ec5e2b1
MeriemBoujamza <kazahayakun@gmail.com>
2021-12-25 15:18:25 +0100

    -filling user profile from db -you can now edit your profile

--- app/src/main/java/ma/ensaf/veryempty/activities/ActivityEditPersInfo.java
+++ app/src/main/java/ma/ensaf/veryempty/activities/ActivityEditPersInfo.java
@@ -3,32 +3,54 @@ package ma.ensaf.veryempty.activities;
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Base64;
+import android.util.Patterns;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.RadioGroup;
+import android.widget.Toast;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.core.content.ContextCompat;
 import androidx.databinding.DataBindingUtil;
 
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.io.ByteArrayOutputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.Arrays;
+import java.util.HashMap;
+
 import ma.ensaf.veryempty.R;
+import ma.ensaf.veryempty.databinding.ActivityEditPersInfoBinding;
 import ma.ensaf.veryempty.databinding.ActivityRegisterDonorBinding;
+import ma.ensaf.veryempty.databinding.ActivitySignUpBinding;
+import ma.ensaf.veryempty.utils.Constants;
+import ma.ensaf.veryempty.utils.PreferenceManager;
 
 public class ActivityEditPersInfo extends BaseActivity {
 
-    private static final String TAG = ActivityRegisterDonor.class.getSimpleName();
+    private static final String TAG = SignUpActivity.class.getSimpleName();
+    private ActivityEditPersInfoBinding binding;
+    private String encodedImage;
+    private PreferenceManager preferenceManager;
 
-    ActivityRegisterDonorBinding binding;
-
-    private View parent_view;
-    boolean maleGenderPressed = false;
-    boolean femaleGenderPressed = false;
-
-    String selectedSex = null;
+    String[] cities = {"Select City","Casablanca", "Fes", "Meknes", "Rabat", "Tangier", "Oujda","Marrakech","Tetuan","Laayoune"};
 
     public static void start(Context context) {
-        Intent intent = new Intent(context, ActivityRegisterDonor.class);
+        Intent intent = new Intent(context, ActivityEditPersInfo.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
         context.startActivity(intent);
     }
@@ -38,136 +60,153 @@ public class ActivityEditPersInfo extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = DataBindingUtil.setContentView(this, R.layout.activity_edit_pers_info);
-        parent_view = findViewById(android.R.id.content);
+        preferenceManager = new PreferenceManager(getApplicationContext());
 
         initToolbar(binding.toolbar,true);
         setToolbarTitle(null);
+        //Lisetners
+        initViews();
+        setListeners();
+        ArrayAdapter citiesAdapter = new ArrayAdapter<>(this, R.layout.support_simple_spinner_dropdown_item, cities);
+        citiesAdapter.setDropDownViewResource(R.layout.spinner_dropdown_item);
+        binding.citySpinner.setAdapter(citiesAdapter);
+        binding.citySpinner.setSelection(Arrays.asList(cities).indexOf(preferenceManager.getString(Constants.KEY_CITY)),false);
+        binding.citySpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                if(position != 0){
+                    preferenceManager.putString(Constants.KEY_CITY,parent.getItemAtPosition(position).toString());
+                }
+            }
 
-        // on click male gender
-        binding.genderMaleImageView.setOnClickListener(v -> {
-            // toggle button
-            toggleGenderButtonStates(true);
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
         });
 
-        // on click female gender
-        binding.genderFemaleImageView.setOnClickListener(v -> {
-            // toggle button
-            toggleGenderButtonStates(false);
-        });
-
-        // listener for blood group radio groups
-        binding.bloodGroupRadioGroup1.setOnCheckedChangeListener(listener);
-        binding.bloodGroupRadioGroup2.setOnCheckedChangeListener(listener);
-        binding.bloodGroupRadioGroup3.setOnCheckedChangeListener(listener);
-        binding.bloodGroupRadioGroup4.setOnCheckedChangeListener(listener);
-        binding.bloodGroupRadioGroup5.setOnCheckedChangeListener(listener);
-        binding.bloodGroupRadioGroup6.setOnCheckedChangeListener(listener);
-        binding.bloodGroupRadioGroup7.setOnCheckedChangeListener(listener);
-        binding.bloodGroupRadioGroup8.setOnCheckedChangeListener(listener);
-
-        // show next screen
-        binding.buttonRegisterDonor.setOnClickListener(v -> {
-            ActivityRequestBlood.start(activityContext);
-        });
-
-//        Tools.systemBarLollipopTransparent(this);
     }
 
-    private void toggleGenderButtonStates(boolean isMale) {
-        if(isMale){
-            selectedSex = "male";
-            // set male views
-            binding.genderMaleImageView.setImageState(new int[] {android.R.attr.state_pressed},true);
-            binding.genderMaleImageView.setColorFilter(ContextCompat.getColor(activityContext, R.color.white), android.graphics.PorterDuff.Mode.MULTIPLY);
-            binding.genderMaleTextView.setTextColor(ContextCompat.getColor(activityContext,R.color.be_hero_dark));
+    private void initViews() {
+        binding.nameInput.setText(preferenceManager.getString(Constants.KEY_NAME));
+        binding.phoneInput.setText(preferenceManager.getString(Constants.KEY_PHONE));
+        binding.emailInput.setText(preferenceManager.getString(Constants.KEY_EMAIL));
+        binding.textAddImage.setVisibility(View.INVISIBLE);
+        byte[] bytes = Base64.decode(preferenceManager.getString(Constants.KEY_IMAGE),Base64.DEFAULT);
+        Bitmap bitmap = BitmapFactory.decodeByteArray(bytes,0,bytes.length);
+        binding.imageProfile.setImageBitmap(bitmap);
+        encodedImage = preferenceManager.getString(Constants.KEY_IMAGE);
+    }
 
-            // reset female views
-            binding.genderFemaleImageView.setImageState(new int[] {-android.R.attr.state_pressed},true);
-            binding.genderFemaleImageView.setColorFilter(ContextCompat.getColor(activityContext, R.color.be_hero_dark_grey), android.graphics.PorterDuff.Mode.MULTIPLY);
-            binding.genderFemaleTextView.setTextColor(ContextCompat.getColor(activityContext,R.color.be_hero_dark_grey));
-        }else{
-            selectedSex = "female";
-            // reset male views
-            binding.genderMaleImageView.setImageState(new int[] {-android.R.attr.state_pressed},true);
-            binding.genderMaleImageView.setColorFilter(ContextCompat.getColor(activityContext, R.color.be_hero_dark_grey), android.graphics.PorterDuff.Mode.MULTIPLY);
-            binding.genderMaleTextView.setTextColor(ContextCompat.getColor(activityContext,R.color.be_hero_dark_grey));
-            // set female views
-            binding.genderFemaleImageView.setImageState(new int[] {android.R.attr.state_pressed},true);
-            binding.genderFemaleImageView.setColorFilter(ContextCompat.getColor(activityContext, R.color.white), android.graphics.PorterDuff.Mode.MULTIPLY);
-            binding.genderFemaleTextView.setTextColor(ContextCompat.getColor(activityContext,R.color.be_hero_dark));
+    private void setListeners() {
+        binding.UpdateBtn.setOnClickListener(v -> {
+            if(isValidProfileDetails()) {
+                updateProfile();
+            }
+        });
+
+        binding.imageProfile.setOnClickListener(v -> {
+            Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI );
+            intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+            pickImage.launch(intent);
+        });
+    }
+
+    private void updateProfile() {
+        loading(true);
+        FirebaseFirestore database = FirebaseFirestore.getInstance();
+        DocumentReference documentReference=
+                database.collection(Constants.KEY_COLLECTION_USERS).document(
+                        preferenceManager.getString(Constants.KEY_USER_ID)
+                );
+        documentReference.update(Constants.KEY_NAME,binding.nameInput.getText().toString(),
+                Constants.KEY_PHONE,binding.phoneInput.getText().toString(),
+                Constants.KEY_CITY,binding.citySpinner.getSelectedItem().toString(),
+                Constants.KEY_EMAIL,binding.emailInput.getText().toString(),
+                Constants.KEY_IMAGE,encodedImage)
+                .addOnSuccessListener(unused -> {
+                    preferenceManager.putString(Constants.KEY_NAME, binding.nameInput.getText().toString());
+                    preferenceManager.putString(Constants.KEY_PHONE, binding.phoneInput.getText().toString());
+                    preferenceManager.putString(Constants.KEY_CITY, binding.citySpinner.getSelectedItem().toString());
+                    preferenceManager.putString(Constants.KEY_EMAIL, binding.emailInput.getText().toString());
+                    preferenceManager.putString(Constants.KEY_IMAGE, encodedImage);
+                    loading(false);
+                    Intent intent= new Intent(getApplicationContext(),ActivityHome.class);
+                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                    startActivity(intent);
+                    finish();
+                })
+                .addOnFailureListener(e -> {
+                    loading(false);
+                    showToast("Unable to update profile",false);
+                });
+    }
+
+    private boolean isValidProfileDetails() {
+        if (binding.nameInput.getText().toString().trim().isEmpty()) {
+            showToast("enter name",false);
+            return false;
+        } else if (binding.emailInput.getText().toString().trim().isEmpty()) {
+            showToast("enter email",false);
+            return false;
+        } else if (!Patterns.EMAIL_ADDRESS.matcher(binding.emailInput.getText().toString()).matches()) {
+            showToast("enter valid email",false);
+            return false;
+        } else if (binding.phoneInput.getText().toString().trim().isEmpty() ||
+                !Patterns.PHONE.matcher(binding.phoneInput.getText().toString()).matches()) {
+            showToast("enter valid phone number",false);
+            return false;
+        } else if (binding.citySpinner.getSelectedItemPosition() == 0) {
+            showToast("select a city",false);
+            return false;
+        } else {
+            return true;
         }
     }
 
-    private RadioGroup.OnCheckedChangeListener listener = (group, checkedId) -> {
-        switch (group.getId()){
-            case R.id.blood_group_radio_group_1:
-                resetRadios(group.getId(),R.id.blood_group_radio_group_2,R.id.blood_group_radio_group_3,R.id.blood_group_radio_group_4,R.id.blood_group_radio_group_5,R.id.blood_group_radio_group_6,R.id.blood_group_radio_group_6,R.id.blood_group_radio_group_8);
-                break;
-            case R.id.blood_group_radio_group_2:
-                resetRadios(group.getId(),R.id.blood_group_radio_group_1,R.id.blood_group_radio_group_3,R.id.blood_group_radio_group_4,R.id.blood_group_radio_group_5,R.id.blood_group_radio_group_6,R.id.blood_group_radio_group_6,R.id.blood_group_radio_group_8);
-                break;
-            case R.id.blood_group_radio_group_3:
-                resetRadios(group.getId(),R.id.blood_group_radio_group_1,R.id.blood_group_radio_group_2,R.id.blood_group_radio_group_4,R.id.blood_group_radio_group_5,R.id.blood_group_radio_group_6,R.id.blood_group_radio_group_6,R.id.blood_group_radio_group_8);
-                break;
-            case R.id.blood_group_radio_group_4:
-                resetRadios(group.getId(),R.id.blood_group_radio_group_1,R.id.blood_group_radio_group_2,R.id.blood_group_radio_group_3,R.id.blood_group_radio_group_5,R.id.blood_group_radio_group_6,R.id.blood_group_radio_group_6,R.id.blood_group_radio_group_8);
-                break;
-            case R.id.blood_group_radio_group_5:
-                resetRadios(group.getId(),R.id.blood_group_radio_group_1,R.id.blood_group_radio_group_2,R.id.blood_group_radio_group_3,R.id.blood_group_radio_group_4,R.id.blood_group_radio_group_6,R.id.blood_group_radio_group_6,R.id.blood_group_radio_group_8);
-                break;
-            case R.id.blood_group_radio_group_6:
-                resetRadios(group.getId(),R.id.blood_group_radio_group_1,R.id.blood_group_radio_group_2,R.id.blood_group_radio_group_3,R.id.blood_group_radio_group_4,R.id.blood_group_radio_group_5,R.id.blood_group_radio_group_7,R.id.blood_group_radio_group_8);
-                break;
-            case R.id.blood_group_radio_group_7:
-                resetRadios(group.getId(),R.id.blood_group_radio_group_1,R.id.blood_group_radio_group_2,R.id.blood_group_radio_group_3,R.id.blood_group_radio_group_4,R.id.blood_group_radio_group_5,R.id.blood_group_radio_group_6,R.id.blood_group_radio_group_8);
-                break;
-            case R.id.blood_group_radio_group_8:
-                resetRadios(group.getId(),R.id.blood_group_radio_group_1,R.id.blood_group_radio_group_2,R.id.blood_group_radio_group_3,R.id.blood_group_radio_group_4,R.id.blood_group_radio_group_5,R.id.blood_group_radio_group_6,R.id.blood_group_radio_group_7);
-                break;
+
+    private void loading(Boolean isLoading)  {
+        if(isLoading) {
+            binding.UpdateBtn.setVisibility(View.INVISIBLE);
+            binding.progressBar.setVisibility(View.VISIBLE);
+        } else {
+            binding.UpdateBtn.setVisibility(View.VISIBLE);
+            binding.progressBar.setVisibility(View.INVISIBLE);
         }
-    };
-
-    private void resetRadios(int checkedId, int radio_group_1, int radio_group_2, int radio_group_3, int radio_group_4, int radio_group_5
-            , int radio_group_6, int radio_group_7) {
-
-        // remove the listeners before clearing so we don't throw an exception
-        ((RadioGroup)findViewById(radio_group_1)).setOnCheckedChangeListener(null);
-        ((RadioGroup)findViewById(radio_group_2)).setOnCheckedChangeListener(null);
-        ((RadioGroup)findViewById(radio_group_3)).setOnCheckedChangeListener(null);
-        ((RadioGroup)findViewById(radio_group_4)).setOnCheckedChangeListener(null);
-        ((RadioGroup)findViewById(radio_group_5)).setOnCheckedChangeListener(null);
-        ((RadioGroup)findViewById(radio_group_6)).setOnCheckedChangeListener(null);
-        ((RadioGroup)findViewById(radio_group_7)).setOnCheckedChangeListener(null);
-        // clear all the other radion groups
-        ((RadioGroup)findViewById(radio_group_1)).clearCheck();
-        ((RadioGroup)findViewById(radio_group_2)).clearCheck();
-        ((RadioGroup)findViewById(radio_group_3)).clearCheck();
-        ((RadioGroup)findViewById(radio_group_4)).clearCheck();
-        ((RadioGroup)findViewById(radio_group_5)).clearCheck();
-        ((RadioGroup)findViewById(radio_group_6)).clearCheck();
-        ((RadioGroup)findViewById(radio_group_7)).clearCheck();
-        //reset the listeners
-        ((RadioGroup)findViewById(radio_group_1)).setOnCheckedChangeListener(listener);
-        ((RadioGroup)findViewById(radio_group_2)).setOnCheckedChangeListener(listener);
-        ((RadioGroup)findViewById(radio_group_3)).setOnCheckedChangeListener(listener);
-        ((RadioGroup)findViewById(radio_group_4)).setOnCheckedChangeListener(listener);
-        ((RadioGroup)findViewById(radio_group_5)).setOnCheckedChangeListener(listener);
-        ((RadioGroup)findViewById(radio_group_6)).setOnCheckedChangeListener(listener);
-        ((RadioGroup)findViewById(radio_group_7)).setOnCheckedChangeListener(listener);
     }
+    private String encodedImage(Bitmap bitmap) {
+        int previewWidth =150;
+        int previewHeight =bitmap.getHeight() * previewWidth/bitmap.getWidth();
+        Bitmap previewBitmap = Bitmap.createScaledBitmap(bitmap,previewWidth,previewHeight,false);
+        ByteArrayOutputStream byteArrayOutputStream= new ByteArrayOutputStream();
+        previewBitmap.compress(Bitmap.CompressFormat.JPEG,50,byteArrayOutputStream);
+        byte[] bytes = byteArrayOutputStream.toByteArray();
+        return Base64.encodeToString(bytes, Base64.DEFAULT);
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        return true;
     }
+    //Getting a result from an activity (camera or gallery app in our example)
+    private final ActivityResultLauncher<Intent> pickImage =  registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            result -> {
+                if (result.getResultCode() == RESULT_OK) {
+                    if(result.getData() != null) {
+                        Uri imageUri = result.getData().getData();
+                        try {
+                            InputStream inputStream= getContentResolver().openInputStream(imageUri);
+                            Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
+                            binding.imageProfile.setImageBitmap(bitmap);
+                            binding.textAddImage.setVisibility(View.GONE);
+                            encodedImage = encodedImage(bitmap);
+                        } catch(FileNotFoundException e) {
+                            e.printStackTrace();
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        if (item.getItemId() == android.R.id.home) {
-            onBackPressed();
-        }
-        return super.onOptionsItemSelected(item);
-    }
+                        }
+                    }
+                }
+
+            }
+    );
+
+
 }
 
--- app/src/main/java/ma/ensaf/veryempty/activities/ActivityPersProfile.java
+++ app/src/main/java/ma/ensaf/veryempty/activities/ActivityPersProfile.java
@@ -1,45 +1,44 @@
 package ma.ensaf.veryempty.activities;
-/*
-* Yo i reversed all my changes here this basically the user profile java
-* but u can edit here the reqs to get current session info
-* when u done connect it to the nav bar f end of home activity
-* The register like activity is ActivityEditPersInfo
-* */
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.util.Base64;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
 
 
 import androidx.databinding.DataBindingUtil;
 import androidx.recyclerview.widget.LinearLayoutManager;
+
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import ma.ensaf.veryempty.R;
 import ma.ensaf.veryempty.adapters.PostsAdapter;
-import ma.ensaf.veryempty.data.Constants;
 import ma.ensaf.veryempty.databinding.ActivityPersProfileBinding;
 import ma.ensaf.veryempty.models.Posts;
 import ma.ensaf.veryempty.models.Users;
+import ma.ensaf.veryempty.utils.Constants;
+import ma.ensaf.veryempty.utils.PreferenceManager;
 
 public class ActivityPersProfile extends BaseActivity {
 
     private static final String TAG = ActivityPersProfile.class.getSimpleName();
-
     ActivityPersProfileBinding binding;
+    private PreferenceManager preferenceManager;
 
-    private View parent_view;
-    private PostsAdapter postsAdapter;
-    private Users parsedUserObj;
 
     public static void start(Context context, Users obj) {
         Intent intent = new Intent(context, ActivityPersProfile.class);
-        intent.putExtra(Constants.USER_EXTRA_OBJECT, obj);
+        intent.putExtra(ma.ensaf.veryempty.data.Constants.USER_EXTRA_OBJECT, obj);
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
         context.startActivity(intent);
     }
@@ -47,74 +46,33 @@ public class ActivityPersProfile extends BaseActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_user_profile);
-        parent_view = findViewById(android.R.id.content);
-
-        // deserialize object from string to object class.
-        parsedUserObj = (Users) getIntent().getSerializableExtra(Constants.USER_EXTRA_OBJECT);
-
-        if (parsedUserObj == null) {
-            finish();
-        }
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_pers_profile);
+        preferenceManager = new PreferenceManager(getApplicationContext());
 
         initToolbar(binding.toolbar,true);
 
         // show the data in the views
         initViews();
+        setListeners();
+    }
 
-        bindRecyclerView();
+    private void setListeners() {
+        TextView EditBtn = findViewById(R.id.pers_donor_type_text_view);
+        EditBtn.setOnClickListener(v -> {
+            startActivity(new Intent(getApplicationContext(), ActivityEditPersInfo.class));
+            finish();
+                }
+        );
     }
 
     private void initViews() {
-        binding.usernameTextView.setText(parsedUserObj.getName());
-        binding.persContentTop.persImageView.setImageResource(parsedUserObj.getImage());
-        binding.persContentTop.persLocationTextView.setText(parsedUserObj.getLocation());
+        binding.usernameTextView.setText(preferenceManager.getString(Constants.KEY_NAME));
+        byte[] bytes = Base64.decode(preferenceManager.getString(Constants.KEY_IMAGE),Base64.DEFAULT);
+        Bitmap bitmap = BitmapFactory.decodeByteArray(bytes,0,bytes.length);
+        binding.persContentTop.persImageView.setImageBitmap(bitmap);
+        binding.persContentTop.persLocationTextView.setText(preferenceManager.getString(Constants.KEY_CITY));
         binding.persContentTop.persDescriptionTextView.setText(activityContext.getString(R.string.medium_lorem_ipsum));
+
     }
 
-    private void bindRecyclerView() {
-        // show the list of recent activity
-        binding.recentActivityRecyclerView.setLayoutManager(new LinearLayoutManager(activityContext));
-        binding.recentActivityRecyclerView.setHasFixedSize(true);
-        binding.recentActivityRecyclerView.setNestedScrollingEnabled(false);
-        //set data and list adapter
-        postsAdapter = new PostsAdapter(activityContext, null);
-        binding.recentActivityRecyclerView.setAdapter(postsAdapter);
-    }
-
-    private void generateListForRecentActivity(List<Posts> posts) {
-        // Create new list of posts
-        // by filtering the post related to this user
-        List<Posts> newPostsList = new ArrayList<>();
-
-        for (Posts posts1:posts) {
-            String str_user_id = String.valueOf(posts1.getUser().getId());
-            if (str_user_id.toLowerCase().equalsIgnoreCase(String.valueOf(parsedUserObj.getId()))) {
-                newPostsList.add(posts1);
-            }
-        }
-
-        // refresh the adapter
-        postsAdapter.setPostsList(newPostsList);
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        generateListForRecentActivity(Constants.getPosts(activityContext));
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        if(item.getItemId() == android.R.id.home){
-            onBackPressed();
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(final Menu menu) {
-        return true;
-    }
 }
